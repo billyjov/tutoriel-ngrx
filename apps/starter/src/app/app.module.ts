f6e55256a8aa9ee8f093a4c16edd1765c3e797ee
Billy Lando <billy_lando@yahoo.com>
2022-04-22 14:24:10 +0200

    feat: add part-6

--- apps/starter/src/app/app.module.ts
+++ apps/starter/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
